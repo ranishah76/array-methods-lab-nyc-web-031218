86bca77e93820804ad83b7ceed965c82dc981486
Rani Shah <rani76@gmail.com>
2018-02-26 21:06:56 +0000

    automatically backed up by learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -7,17 +7,17 @@ def using_sort(array)
 end
 
 def using_reverse(array)
-  return array.reverse 
+  return array.reverse
 end
 
 def using_first(array)
-  return array.first 
+  return array.first
 end
 
 def using_last(array)
-  return array.last 
+  return array.last
 end
 
 def using_size(array)
- array.size 
+ array.size
 end
